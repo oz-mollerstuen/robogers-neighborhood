3a62ff599f1b0e7089b0bd00080bb67baf8a2849
Lucas Mollerstuen <mollerstuen.works@gmail.com>
2022-10-28 16:52:47 -0700

    WIP: does not function

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@ window.addEventListener("load", function(event) {
   event.preventDefault();
   const form = document.getElementById("numbers");
   form.addEventListener("submit", wordRedo);
-  });
-
+  
+});
 
 //const button = document.querySelector("#button");
 //button.addEventListener("submit", clickhandler);
@@ -14,30 +14,44 @@ window.addEventListener("load", function(event) {
 
 
 //----------------------//-----------------//----------------//--------------//--------------//
+let newArray = [];
 
-let newArray = []
-let stApart = document.querySelector("#input1");
-function wordRedo(element) {
-  console.log("ffs");
-  for (i = 0; i <= stApart; i++) {
-    const element = i.toString();
-    if (element.includes(3)) {
+
+function wordRedo(event) {
+  event.preventDefault();
+
+  let stApart = document.querySelector("input#input1").value;
+  stApart = stApart.toString().split(" ");
+
+  for (let i = 0; i <= stApart; i++) {
+    //console.log(stApart[i]);
+    //const element = i.toString();
+   
+    if (stApart[i] == 3) {
+      console.log('it has a 3');
       newArray.push("Won't *zap* you be my...error...neighbor?");
     }
-    else if (element.includes(2)) {
+    else if (stApart[i] == 2) {
+      console.log('it has a 2');
       newArray.push("Boop!");
     }
-    else if (element.includes(1)) {
+    else if (stApart[i] == 1) {
+      console.log('it has a 1');
       newArray.push("Beep?");
     }
     else {
-      newArray.push(element);
+      newArray.push(stApart[i]);
+      
     }
-    return newArray
-    
+    let output1 = document.getElementById("p#output")
+    output1 = newArray.toString
+
   }
+      console.log(newArray);
 }
  
-let output1 = document.getElementById("output")
-output1 = newArray
 
+//}
+ 
+  
+  
